c26fc0dbe4561041e7aa47c07484e66f36e99807
shahargl <shaharglazner@gmail.com>
2025-02-16 13:35:43 +0200

    feat: wip

--- keep-ui/components/navbar/NoiseReductionLinks.tsx
+++ keep-ui/components/navbar/NoiseReductionLinks.tsx
@@ -2,7 +2,7 @@
 
 import { Subtitle } from "@tremor/react";
 import { LinkWithIcon } from "components/LinkWithIcon";
-import { Mapping, Rules, Workflows, ExportIcon } from "components/icons";
+import { Mapping, Rules, ExportIcon } from "components/icons";
 import { Session } from "next-auth";
 import { Disclosure } from "@headlessui/react";
 import { IoChevronUp } from "react-icons/io5";
@@ -50,11 +50,6 @@ export const NoiseReductionLinks = ({ session }: NoiseReductionLinksProps) => {
           </LinkWithIcon>
         </li>
         <li>
-          <LinkWithIcon href="/workflows" icon={Workflows}>
-            <Subtitle>Workflows</Subtitle>
-          </LinkWithIcon>
-        </li>
-        <li>
           <LinkWithIcon
             href="/topology"
             icon={TbTopologyRing}
@@ -84,7 +79,7 @@ export const NoiseReductionLinks = ({ session }: NoiseReductionLinksProps) => {
         <li>
           <AILink></AILink>
         </li>
-    </Disclosure.Panel>
+      </Disclosure.Panel>
     </Disclosure>
   );
 };
